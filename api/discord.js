88fae80b15760552ac5a484e859e0e62c5815165
Gamer Ben <136914442+gamerbenyt@users.noreply.github.com>
2026-01-11 10:07:35 -0500

    Update REDIRECT_URI for Discord callback

--- api/discord.js
+++ api/discord.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
   const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
   
   // The redirect_uri must be the FULL URL of your callback file
-  const REDIRECT_URI = encodeURIComponent("https://copper-api.vercel.app/api/discord_callback");
+  const REDIRECT_URI = encodeURIComponent("https://copperclient-api.vercel.app/api/discord_callback");
 
   const discordUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=identify&state=${token}`;
   
